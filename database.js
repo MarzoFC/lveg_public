640782cb2213498f2bdd3291ac9c4e5bccab2d44
MarzoFC <marzoforment@gmail.com>
2021-02-10 23:10:34 +0100

    sec

--- database.js
+++ database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 //mysql --host=us-cdbr-east-03.cleardb.com --user=b998edc31cacd7 --password=3243e73b --reconnect heroku_50e309c4e32044b
 
 const config = {
-    host: 'us-cdbr-east-03.cleardb.com',
-    user: 'b998edc31cacd7',
-    password: '3243e73b',
-    database: 'heroku_50e309c4e32044b',
+    host: '---',
+    user: '----',
+    password: '---',
+    database: '---',
 };
 
 const pool = mysql.createPool(config);
